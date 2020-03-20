0ecbc6dc74798126ee7e3d17c82a6e3c604a6779
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-03-20 15:57:11 -0700

    Created GradeTable class and constructor function

--- components/app.js
+++ components/app.js
@@ -1,14 +1,29 @@
-class APP {
+class App {
   constructor() {
-
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
   }
 
   handleGetGradesError(error){
     console.error(error);
-  },
+  }
 
   handleGetGradesSuccess(grades) {
     console.log(grades);
-  },
+  }
+
+  getGrades(){
+    $.ajax({
+      method:"GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: { "X-Access-Token": "QoYrtKFL" },
+      success: this.handleGetGradesSuccess,
+      error:this.handleGetGradesError
+    })
+  }
+
+  start() {
+    this.getGrades();
+  }
 
 }
