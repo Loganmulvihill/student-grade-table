bda08441425cc6bebecdff99de0c6bc9fb504e88
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-03-21 15:15:43 -0700

    added page header component

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
  var table = document.querySelector('table');
 
 var gradeTable = new GradeTable(table);
+var header = document.querySelector('header');
 
-var app = new App(gradeTable);
+var pageHeader = new PageHeader(header);
+
+var app = new App(gradeTable,pageHeader);
 
 app.start();
