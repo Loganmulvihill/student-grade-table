03dfd0a5fd1fb3fd69478b3488d9a61dd73136a4
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-03-22 14:51:34 -0700

    completed step 1 of post new grades

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+ var form = document.querySelector('form');
+var gradeForm = new GradeForm(form);
  var table = document.querySelector('table');
 
 var gradeTable = new GradeTable(table);
 var header = document.querySelector('header');
 
 var pageHeader = new PageHeader(header);
-
+// var gradeForm = new GradeForm(form);
 var app = new App(gradeTable,pageHeader);
 
 app.start();
