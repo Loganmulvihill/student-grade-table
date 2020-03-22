7a806c07f2d04266cef892b6fea963530b91f54a
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-03-22 16:00:42 -0700

    fixed start method to include onSubmit function methodology

--- components/app.js
+++ components/app.js
@@ -5,6 +5,9 @@ class App {
     this.gradeForm = gradeForm;
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    this.createGrade = this.createGrade.bind(this);
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
   }
 
   handleGetGradesError(error){
@@ -34,6 +37,32 @@ class App {
 
   start() {
     this.getGrades();
+    // this.createGrade;
+    this.gradeForm.onSubmit(this.createGrade);
+    // this.createGrade = this.gradeForm.onSubmit();
   }
 
+
+  createGrade(name,course,grade) {
+    console.log(name,course,grade)
+
+    $.ajax({
+      method: "POST",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      Data:{name,course,grade},
+      headers: { "X-Access-Token": "QoYrtKFL" },
+      success: this.handleCreateGradeSuccess,
+      error: this.handleCreateGradeError
+    })
+
+
+  }
+
+  handleCreateGradeError(error) {
+    console.error(error);
+  }
+
+  handleCreateGradeSuccess(){
+    this.getGrades();
+  }
 }
