b257b596cbd3b35250d1a62454d1744f526f20e6
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-03-23 11:08:42 -0700

    modified GradeTable's constructor, the update method to conditionally show or hide the p element, query the dom for the p element and pass it as an argument to the GradeTable class

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,13 +1,15 @@
 class GradeTable {
 
-  constructor(tableElement){
+  constructor(tableElement, noGradesElement){
     this.tableElement = tableElement;
+    this.noGradesElement = noGradesElement;
   }
 
   updateGrades(grades){
     var table = this.tableElement.querySelector('tbody');
+    var pElement = this.querySelector('no-grades-text')
     table.innerHTML = '';
-    console.log(table);
+    // console.log(table);
     for( var i = 0; i < grades.length; i++){
       var tr = document.createElement('tr');
 
@@ -19,17 +21,57 @@ class GradeTable {
 
       var td3 = document.createElement('td');
       td3.textContent = grades[i].grade;
+      this.renderGradeRow(grades,this.deleteGrade();)
+      // var button = document.createElement('button');
+      // button.textContent = "Delete";
+      // button.className += "btn btn-danger"
 
       tr.appendChild(td1);
       tr.appendChild(td2);
       tr.appendChild(td3);
-
+      // tr.appendChild(button);
       table.appendChild(tr);
     }
+
+    if(grades.length < 0) {
+      pElement.classList.remove("d-none")
+    } else {pElement.classList.add("d-none")}
   }
 
   onDeleteClick(deleteGrade){
     this.deleteGrade = deleteGrade;
   }
+
+
+
+  renderGradeRow(data,deleteGrade){
+
+    // data = grade;
+
+    var table = this.tableElement.querySelector('tbody');
+    // table.innerHTML = '';
+    // console.log(table);
+    for (var i = 0; i < data.length; i++) {
+      var tr = document.createElement('tr');
+
+      var td = document.createElement('td');
+      var deleteButton = document.createElement('button');
+      deleteButton.textContent = "Delete";
+      deleteButton.className += "btn btn-danger"
+
+      td.appendChild(deleteButton);
+      tr.appendChild(deleteButton);
+      table.appendChild(tr);
+  }
+  deleteButton.addEventListener('click',deleteGrade(data.id));
+  console.log(data);
+
+    return tr;
+
+    function deleteGrade() {
+
+    }
+
+ }
 
 }
